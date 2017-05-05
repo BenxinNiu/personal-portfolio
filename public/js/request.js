29143e01a8f48b656b80c8a5b6fea2600f9a89ef
Benxin Niu <bn2645@mun.ca>
2017-05-05 06:22:30 -0230

    update

---
+++ public/js/request.js
@@ -0,0 +1,74 @@
+
+function acquireInformation(){
+  return{
+    type:$('#category').val(),
+    company:$('#company').val(),
+    title:$('#gender').val(),
+    first_name:$('#first').val(),
+    last_name:$('#last').val(),
+    email:$('#email').val()
+  }
+}
+
+//return true when email is valid
+function validateEmail(email) {
+var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+if (regex.test(email)&&email.length!=0)
+return true;
+else
+return false;
+}
+
+
+function validate(form){
+  var pass=true
+  if(form.email==""||!validateEmail(form.email)){
+    pass=false;
+  $('.email').css('color','red');
+  }
+if(form.company==""){
+  pass=false;
+$('.company').css('color','red');
+}
+if(form.first_name==""){
+  pass=false;
+$('.first').css('color','red');
+}
+if(form.last_name==""){
+  pass=false;
+$('.last').css('color','red');
+}
+return pass;
+}
+
+function send_infor(){
+  var form=acquireInformation();
+  if(!validate(form)){
+    $('.notification').addClass('alert-danger').html("Please Check your form")
+  }
+  else{
+    $.ajax({
+      url:'/lucky',
+      type:'POST',
+      data:form,
+      success:function(result){
+  $('.notification').addClass('alert-success').html("Thank you, You will receive the email once the verification is complete")
+      },
+      error:function(error){
+$('.notification').addClass('alert-danger').html("Ooops.. Server connection lost")
+      }
+    });
+  }
+}
+
+$(document).ready(function(){
+  $('input').focus(function(){
+  $('label').css('color','#383838')
+  var cla='.'+  $(this).attr('id');
+  $(cla).animate({bottom:"0px"},500)
+});
+$('#fire').click(function(){
+document.getElementById('fire').disabled=true;
+send_infor();
+})
+})
---
+++ server.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const express = require('express');
+const fs=require('fs');
+const mongo=require('mongodb').MongoClient;
+const bodyparser=require('body-parser');
+const path = require('path');
+const config = require('./config');
+const mongoURL=config.get('MONGO_URL');
+const app=express();
+
+app.use(express.static(path.join(__dirname,'public')));
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json()); // for parsing application/json
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods","GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+
+
+app.get('/',(req,res)=>{
+res.sendFile(path.join(__dirname,'public','index.html'));
+});
+
+app.get('/request',(req,res)=>{
+res.sendFile(path.join(__dirname,'public','request.html'));
+});
+
+app.get('/project',(req,res)=>{
+res.sendFile(path.join(__dirname,'public','project.html'));
+});
+
+app.post('/lucky',(req,res)=>{
+var data=req.body;
+mongo.connect(mongoURL,(err,db)=>{
+if(err){db.close(); res.sendStatus(500);}
+else{
+var collection=db.collection('requests');
+collection.insert(data,(err,data)=>{
+if (!err){
+  res.send('success');
+  db.close();
+}
+else{
+db.close(); res.sendStatus(500);
+}
+})
+}
+});
+});
+
+
+
+var server=app.listen(process.env.port||'8080',function(){
+  console.log("running on"+server.address().port);
+})
