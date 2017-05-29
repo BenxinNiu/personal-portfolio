93d729f2d90ff06d3742d2f4efc57c717f896868
benxin niu <benxin niu>
2017-05-29 17:51:42 -0230

    improve js file

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,15 @@
+function news(){
+  console.log("news");
+  $(".alert").animate({top: '0px'},1000);
+}
+
 $(document).ready(function(){
   $('.courses').click(function(){
     window.location.href='https://www.mun.ca/regoff/calendar/sectionNo=ENGI-0422';
   })
+setTimeout(news,1000);
+if(window.innerWidth<=990)
+  $('.navbar-custom').css("background-color","black")
+else
+$('.navbar-custom').css("background-color","transparent")
 })
