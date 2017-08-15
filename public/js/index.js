eac9e0638397c6fc6e1e1050116270c593cf1785
Benxin Niu <bn2645@mun.ca>
2017-08-15 18:57:50 -0230

    CHANGED INDEX PAGE LAYOUT

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,35 @@ function news(){
 //  $(".alert").animate({top: '0px'},1000);
 }
 
+function get_user(){
+$.ajax({
+type:"GET",
+url:"https://api.github.com/users/BenxinNiu/repos",
+success: function(data){
+  console.log(data)
+  display_infor(data);
+},
+error: function(err){
+  console.log(err)
+}
+})
+}
+function display_infor(data){
+var length=data.length;
+$(".numOfRepos").html("Toal "+length+" repositories found (excluding private repos)");
+for(var i=1;i<length;i++){
+  if (data[i].name.length>20)
+$('.board').append(" <div class='sm-box col-md-4'><p>"+data[i].name+"</p></div>");
+else
+$('.board').append(" <div class='sm-box col-md-2'><p>"+data[i].name+"</p></div>");
+}
+}
+
+
+
+
 $(document).ready(function(){
+  get_user();
   $('.courses').click(function(){
     window.location.href='https://www.mun.ca/regoff/calendar/sectionNo=ENGI-0422';
   })
