30d0b49aa5cdc91a7212bb8bf13840f0cbe21e8c
Benxin Niu <bn2645@mun.ca>
2017-08-15 15:26:05 -0230

    changed cover

--- public/js/index.js
+++ public/js/index.js
@@ -7,6 +7,13 @@ $(document).ready(function(){
   $('.courses').click(function(){
     window.location.href='https://www.mun.ca/regoff/calendar/sectionNo=ENGI-0422';
   })
+$(".search-area").slideUp(0);
+
+$('.expand-search').click(function(){
+  $(".search-area").slideToggle()
+})
+
+
 setTimeout(news,1000);
 if(window.innerWidth<=990)
 $('.search-bar').addClass('hidden')
